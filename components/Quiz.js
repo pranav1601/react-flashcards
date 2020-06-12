f8545b64c14a4a9d7b39975b340ae82f31b49dac
pranav1601 <pranav.agarw@gmail.com>
2020-06-12 23:32:29 +0530

    updated

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Platform} from 'react-native';
 import IOSPlat from './IOSPlat';
-import {setLocalNotification,clearLocalNotification } from '../utils/api';
+import {notification } from '../utils/api';
 
 export class Quiz extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -12,7 +12,7 @@ export class Quiz extends Component {
   };
   componentDidMount() {
     if(Platform.OS==='android' || Platform.OS==='ios'){
-      clearLocalNotification().then(setLocalNotification)
+      notification()
     }    
   }
   render() {
--- utils/api.js
+++ utils/api.js
@@ -1,9 +1,8 @@
-import {AsyncStorage,Platform} from 'react-native'
+import {AsyncStorage} from 'react-native'
 import {decks} from './DATA'
 import Constants from 'expo-constants'
 import * as Permissions from 'expo-permissions'
 import {Notifications} from 'expo'
-
 
 
 const STORAGE_KEY='Flashcards:decks'
@@ -77,78 +76,47 @@ export function attemptedQuiz() {
     } )
 }
 
-const NOTIFICATION_KEY = 'MobileFlashcard:notifications';
-const CHANNEL_ID = 'DailyReminder';
-
-export function clearLocalNotification() {
-    return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
-      Notifications.cancelAllScheduledNotificationsAsync
-    );
-  }
-
-  function createNotification() {
-    return {
-      title: 'Mobile Flashcards Reminder',
-      body: "👋 Don't forget to study for today!",
-      ios: {
-        sound: true
-      },
-      android: {
-        channelId: CHANNEL_ID,
-        sticky: false,
-        color: 'red'
-      }
-    };
-  }
-  
-  function createChannel() {
-    return {
-      name: 'Daily Reminder',
-      description: 'This is a daily reminder for you to study your flashcards.',
-      sound: true,
-      priority: 'high'
-    };
-  }
-  
-  export function setLocalNotification() {
-    AsyncStorage.getItem(NOTIFICATION_KEY)
-      .then(JSON.parse)
-      .then(data => {
-        // if (true) {
-        if (data === null) {
-          Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
-            // console.log('got in');
-            // console.log('data', data);
-            if (status === 'granted') {
-              // Notifications.presentLocalNotificationAsync(createNotification());
-              Notifications.createChannelAndroidAsync(CHANNEL_ID, createChannel())
-                .then(val => console.log('channel return:', val))
-                .then(() => {
-                  Notifications.cancelAllScheduledNotificationsAsync();
-  
-                  const tomorrow = new Date();
-                  // 2 minute from now
-                  // tomorrow.setTime(tomorrow.getTime() + 2 * 60000);
-  
-                  tomorrow.setDate(tomorrow.getDate() + 1);
-                  tomorrow.setHours(20);
-                  tomorrow.setMinutes(0);
-  
-                  Notifications.scheduleLocalNotificationAsync(
-                    createNotification(),
-                    {
-                      time: tomorrow,
-                      repeat: 'day'
-                    }
-                  );
-  
-                  AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
-                })
-                .catch(err => {
-                  console.log('err', err);
-                });
+export function notification() {
+    Permissions.askAsync(Permissions.NOTIFICATIONS).then(({status})=>{
+            if(Constants.isDevice && status==='granted'){
+                Notifications.cancelAllScheduledNotificationsAsync().then((result) => {
+                        const handleNotification = ({notificationId})=>{
+                            this.attemptedQuiz().then((result)=>{
+                                    (result) && Notifications.dismissNotificationAsync(notificationId)
+                                } )
+                        }
+                        Notifications.addListener( handleNotification );
+                        if ( Platform.OS === 'android' ) {
+                            Notifications.createChannelAndroidAsync( 'card-reminder', {
+                                name: 'card-reminder',
+                                sound: true,
+                                priority: 'max',
+                            } )
+                        }
+                        const localNotification = {
+                            title:'Reminder for quiz',
+                            body:`Attempt the quiz please!`,
+                            ios:{ sound: true},
+                            android:{
+                                channelId:'card-reminder',
+                                color:"#101057",
+                            }
+                        };
+                        let notificationTime = new Date()
+                        let currTime = notificationTime.getTime()
+                        notificationTime.setHours(18, 0, 0)
+                        scheduleTime = notificationTime.getTime()
+                        if (currTime>scheduleTime){
+                            scheduleTime=scheduleTime+86400000
+                        }
+                        const scheduleOptions = {
+                            time: scheduleTime,
+                            repeat: 'day'
+                        }
+                        Notifications.scheduleLocalNotificationAsync(
+                            localNotification, scheduleOptions
+                        );
+                    } )
             }
-          });
-        }
-      });
-  }
+        } )
+}
